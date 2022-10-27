80734419701bebee96515b8a66fff2d12d136ad5
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2022-10-27 12:09:19 -0400

    enabling js

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -1,16 +1,18 @@
 package edu.temple.basicbrowser
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var urlEditText: EditText
-    lateinit var goButton: Button
+    lateinit var goButton: ImageButton
     lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +23,8 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
-        // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
 
     }
 }
